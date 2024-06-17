eaee1a55b5db6c173629b3010655ae7daccfdc27
ksdb10984847 <102569074+ksdb10984847@users.noreply.github.com>
2024-06-17 18:37:18 +0000

    Add files via upload

---
+++ Inheritanceassignment/Inheritanceassignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InheritanceAssignment
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound(); // Output: Some generic sound
+            dog.MakeSound();           // Output: Bark
+            cat.MakeSound();           // Output: Meow
+
+            Console.ReadLine();
+        }
+    }
+}
