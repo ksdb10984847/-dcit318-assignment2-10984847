6c50cc26309f7cbabcefc6c4184de919a6cfb97b
ksdb10984847 <102569074+ksdb10984847@users.noreply.github.com>
2024-06-17 18:40:56 +0000

    Add files via upload

---
+++ Interfaceassignment/Interfaceassignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InterfaceAssignment
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();       // Output: Car is moving
+            bicycle.Move();   // Output: Bicycle is moving
+
+            Console.ReadLine();
+        }
+    }
+}
